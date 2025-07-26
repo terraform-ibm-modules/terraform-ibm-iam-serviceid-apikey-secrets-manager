bec0d835079a1d3039d7f06f7d0e5a51a07fd894
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-07-26 16:12:42 +0100

    chore(deps): update terraform terraform-ibm-modules/secrets-manager/ibm to v2.6.16 (#292)

--- examples/complete-rotation-policy/main.tf
+++ examples/complete-rotation-policy/main.tf
@@ -27,7 +27,7 @@ module "resource_group" {
 
 module "secrets_manager" {
   source                        = "terraform-ibm-modules/secrets-manager/ibm"
-  version                       = "2.6.15"
+  version                       = "2.6.16"
   existing_sm_instance_crn      = var.existing_sm_instance_crn
   skip_iam_authorization_policy = var.skip_iam_authorization_policy
   resource_group_id             = module.resource_group.resource_group_id
