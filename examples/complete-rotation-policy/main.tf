be7dbde69964fb58bfae88b6868504a9e01d1b62
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-10-17 20:45:35 +0000

    chore(deps): update dependencies

--- examples/complete-rotation-policy/main.tf
+++ examples/complete-rotation-policy/main.tf
@@ -13,7 +13,7 @@ locals {
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.3.0"
+  version = "1.4.0"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -27,7 +27,7 @@ module "resource_group" {
 
 module "secrets_manager" {
   source                        = "terraform-ibm-modules/secrets-manager/ibm"
-  version                       = "2.10.2"
+  version                       = "2.10.3"
   existing_sm_instance_crn      = var.existing_sm_instance_crn
   skip_iam_authorization_policy = var.skip_iam_authorization_policy
   resource_group_id             = module.resource_group.resource_group_id
