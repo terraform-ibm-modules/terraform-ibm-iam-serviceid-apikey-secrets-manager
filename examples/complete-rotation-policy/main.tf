a8633c87963e8cd11e2108d76c44d8712ef757a0
Arya Girish K <arya.girish.k@ibm.com>
2025-11-24 09:50:52 +0530

    fix: resolved precommit error

--- examples/complete-rotation-policy/main.tf
+++ examples/complete-rotation-policy/main.tf
@@ -73,7 +73,7 @@ resource "ibm_iam_service_id" "secret_puller" {
 
 resource "ibm_iam_service_policy" "secret_puller_policy" {
   iam_id = ibm_iam_service_id.secret_puller.iam_id
-  roles          = ["Viewer", "SecretsReader"]
+  roles  = ["Viewer", "SecretsReader"]
 
   resources {
     service              = "secrets-manager"
