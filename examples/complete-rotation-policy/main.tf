09f673deff97f5308e2692f396fc2fa23ddb5577
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-12-06 05:55:05 +0000

    fix(deps): update dependencies

--- examples/complete-rotation-policy/main.tf
+++ examples/complete-rotation-policy/main.tf
@@ -13,7 +13,7 @@ locals {
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.4.0"
+  version = "1.4.1"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -41,7 +41,7 @@ module "secrets_manager" {
 # Additional Secrets-Manager Secret-Group for SERVICE level secrets
 module "secrets_manager_group_acct" {
   source               = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version              = "1.3.19"
+  version              = "1.3.20"
   region               = local.sm_region
   secrets_manager_guid = module.secrets_manager.secrets_manager_guid
   #tfsec:ignore:general-secrets-no-plaintext-exposure
@@ -51,7 +51,7 @@ module "secrets_manager_group_acct" {
 
 module "secrets_manager_group_service" {
   source               = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version              = "1.3.19"
+  version              = "1.3.20"
   region               = local.sm_region
   secrets_manager_guid = module.secrets_manager.secrets_manager_guid
   #tfsec:ignore:general-secrets-no-plaintext-exposure
